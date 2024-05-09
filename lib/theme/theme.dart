d8daa6e4e1a3ce22bbcb3b5802dc3b482cb53b7f
111000279 <michael.sucahyo@gmail.com>
2024-05-09 23:55:20 +0800

    change color

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,27 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
   colorScheme: const ColorScheme.light(
     background: Color(0xFFFFFFFF),
-    onBackground: Color(0xFF000000),
-    primary: Color(0xFF6582AA),
-    secondary: Color(0xFFA3C5F1),
-    tertiary: Color(0xFFB2B7BF),
-  ),
-  primaryTextTheme: GoogleFonts.comfortaaTextTheme(),
-  textTheme: GoogleFonts.poppinsTextTheme(),
-);
-
-ThemeData darkMode = ThemeData(
-  brightness: Brightness.dark,
-  colorScheme: const ColorScheme.dark(
-    background: Color(0xFF000000),
-    onBackground: Color(0xFFFFFFFF),
-    primary: Color(0xFF6582AA),
-    secondary: Color(0xFFA3C5F1),
-    tertiary: Color(0xFFB2B7BF),
+    primary: Color.fromARGB(255, 103, 6, 164),
+    secondary: Color.fromARGB(255, 57, 26, 120),
+    tertiary: Color.fromARGB(255, 137, 79, 190),
   ),
   primaryTextTheme: GoogleFonts.comfortaaTextTheme(),
   textTheme: GoogleFonts.poppinsTextTheme(),
