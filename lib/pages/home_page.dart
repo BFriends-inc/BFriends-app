24e1143010a5ac2a7de5238037476de3cff89a7e
KenjiTECHinc <76623254+KenjiTECHinc@users.noreply.github.com>
2024-05-10 01:48:59 +0800

    Map API - 10:20
    Following Google Map API tutorial of this link: https://youtu.be/M7cOmiSly3Q?si=-hL5_40jro07GXoy
    
    Co-Authored-By: Pakapool <61725943+3normousz@users.noreply.github.com>

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,29 @@
 import 'package:bfriends_app/services/navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  static const LatLng _QingDa = LatLng(137.4223, -122.0848);
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('MAP PAGE');
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(target: _QingDa, zoom: 15),
+      markers: {
+        Marker(
+            markerId: MarkerId("_currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _QingDa),
+      },
+    );
   }
 }
