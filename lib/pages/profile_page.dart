c6429b32630ddb81a9b26451799e3ebe42bdbbd4
KenjiTECHinc <76623254+KenjiTECHinc@users.noreply.github.com>
2024-05-27 00:54:02 +0800

    complete profile page layout
    Completed overall profile page layout.
    
    ToDo: Make provider for user info. and pull from the provider.

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -29,47 +29,191 @@ class ProfilePage extends StatelessWidget {
         //user's profiles displayed here.
         child: Container(
           width: width,
-          alignment: Alignment.center,
-          padding: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 0.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(5.0),
-                    height: 100.0,
-                    width: 100.0,
-                    child: const ClipOval(
-                      clipBehavior: Clip.antiAlias,
-                      child: Placeholder(),
+          alignment: Alignment.topCenter,
+          padding: const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 0.0),
+          child: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(5.0),
+                      height: 100.0,
+                      width: 100.0,
+                      child: ClipOval(
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.asset(
+                          'assets/images/BFriends_logo_full.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                  ),
-                  const Flexible(
-                    child: Placeholder(
-                      //Username display
-                      fallbackHeight: 100.0,
+                    Flexible(
+                      child: Container(
+                        //username display
+                        padding: const EdgeInsets.fromLTRB(10.0, 5.0, 5.0, 5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Username',
+                              style: theme.textTheme.headlineSmall,
+                            ),
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              'Something',
+                              style: theme.textTheme.bodySmall,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 0),
-                child: const Column(children: [
-                  Placeholder(
-                    //Short Bio...
-                    fallbackHeight: 60.0,
-                  ),
-                  Placeholder(
-                    //View preferred language.
-                    fallbackHeight: 60.0,
-                  ),
-                  Placeholder(
-                    //View activities type.
-                    fallbackHeight: 60.0,
-                  ),
-                ]),
-              ),
-            ],
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 0),
+                  child: Column(children: [
+                    Container(
+                      //BIO
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.secondaryContainer,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'About Me',
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                          Text(
+                            'About me mvlajkljdklajd',
+                            style: theme.textTheme.bodySmall,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Joined Since',
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                          Text(
+                            'Date',
+                            style: theme.textTheme.bodySmall,
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      //LANGUAGE
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.secondaryContainer,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'My Preferred Languages',
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: const EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '999',
+                                    style: theme.textTheme.bodySmall,
+                                    textAlign: TextAlign.right,
+                                  ),
+                                  IconButton(
+                                    iconSize:
+                                        theme.textTheme.bodyMedium!.fontSize,
+                                    style: const ButtonStyle(
+                                      splashFactory: NoSplash.splashFactory,
+                                    ),
+                                    onPressed: () {
+                                      debugPrint('view languages pressed.');
+                                    },
+                                    icon: const Icon(
+                                      Icons.arrow_forward_ios_rounded,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      //ACTIVITY
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.secondaryContainer,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'My Interests',
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: const EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '999',
+                                    style: theme.textTheme.bodySmall,
+                                    textAlign: TextAlign.right,
+                                  ),
+                                  IconButton(
+                                    iconSize:
+                                        theme.textTheme.bodyMedium!.fontSize,
+                                    style: const ButtonStyle(
+                                      splashFactory: NoSplash.splashFactory,
+                                    ),
+                                    onPressed: () {
+                                      debugPrint('view activities pressed.');
+                                    },
+                                    icon: const Icon(
+                                      Icons.arrow_forward_ios_rounded,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
           ),
         ),
       ),
