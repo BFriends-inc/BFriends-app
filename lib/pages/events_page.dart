392c30fd3b0444cf86f718cee02896dfc252bf55
Pakapool Boonma <dinodragoz77@gmail.com>
2024-06-08 00:06:23 +0800

    Updated events page
    - Added top app bar
    - Added create an event button, which will pop up a box where user can put event's name and event's date with DatePicker

--- lib/pages/events_page.dart
+++ lib/pages/events_page.dart
@@ -1,10 +1,155 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class EventsPage extends StatelessWidget {
+import 'package:bfriends_app/pages/new_event_page.dart';
+import 'package:bfriends_app/services/navigation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class EventsPage extends StatefulWidget {
   const EventsPage({super.key});
 
   @override
+  _EventsPageState createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+  final _formProfileSetupKey = GlobalKey<FormState>();
+  DateTime? _selectedDate;
+  late StateSetter _setState;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: now,
+      lastDate: DateTime(now.year + 1),
+    );
+    if (pickedDate != null) {
+      _setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
+  }
+
+  void _submitForm() {
+    if (_formProfileSetupKey.currentState!.validate()) {
+      if (_selectedDate == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please select your date of birth')),
+        );
+      } else {
+        _setState(() {
+          _selectedDate = null;
+        });
+        debugPrint('Form submitted');
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('EVENTS PAGE');
+    final theme = Theme.of(context);
+    
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theme.colorScheme.primary,
+        title: Text(
+          'BFriends',
+          style: TextStyle(
+            fontSize: theme.primaryTextTheme.headlineMedium?.fontSize,
+            fontWeight: theme.primaryTextTheme.headlineMedium?.fontWeight,
+            color: theme.colorScheme.onPrimary,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              color: theme.colorScheme.onPrimary,
+              semanticLabel: 'Add a New Event',
+            ),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return StatefulBuilder(builder: (context, setState) {
+                    _setState = setState;
+                     return Dialog(
+                      child: Form(
+                      key: _formProfileSetupKey,
+                        child: Container(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Text(
+                                'New Event',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 16),
+                              const TextField(
+                                decoration: InputDecoration(
+                                  labelText: 'Event Name',
+                                ),
+                              ),
+                              const SizedBox(height: 16),
+                              GestureDetector(
+                                onTap: () => _presentDatePicker(),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: theme.colorScheme.tertiaryContainer,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0,
+                                    horizontal: 12.0,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        _selectedDate == null
+                                            ? 'Event Date'
+                                            : '${_selectedDate!.day.toString().padLeft(2, '0')}/${_selectedDate!.month.toString().padLeft(2, '0')}/${_selectedDate!.year}',
+                                      ),
+                                      IconButton(
+                                        onPressed: _presentDatePicker,
+                                        icon: Icon(
+                                          Icons.calendar_today,
+                                          color: theme.colorScheme.tertiary,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 16),
+                              // save button
+                              ElevatedButton(
+                                onPressed: () {
+                                  _submitForm();
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Save'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
