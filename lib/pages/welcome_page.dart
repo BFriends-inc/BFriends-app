78b00f11443379a0f30dc8a5e6679c5c32a15db1
JoeyyTan <sjoeyt04@gmail.com>
2024-06-19 16:51:27 +0800

    Fixed logo

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -21,83 +21,82 @@ class WelcomeScreen extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
-        child: Align(
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
-              SizedBox(
-                height: 200,
-                child: Center(
-                  child: Image.asset(
-                    //cover the whole box with the pic
-                    'assets/images/whitelogo.png',
-                    fit: BoxFit.cover,
-                  ),
+        child: Column(
+          children: [
+            const SizedBox(height: 200),
+            SizedBox(
+              height: 200,
+              child: Center(
+                child: Image.asset(
+                  'assets/images/whitelogo.png',
+                  fit: BoxFit.cover,
                 ),
               ),
-              const SizedBox(height: 10),
-              Flexible(
-                flex: 8,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 0,
-                    horizontal: 40.0,
-                  ),
-                  child: Center(
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: 'Welcome to BeFriends!\n',
-                            style: TextStyle(
-                              fontSize: 35.0,
-                              fontWeight: FontWeight.w600,
-                              color: theme.colorScheme.onPrimary,
-                            ),
-                          ),
-                          TextSpan(
-                            text: '\nA place to connect with people',
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: theme.colorScheme.onPrimary,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+            ),
+            const SizedBox(height: 25),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 0,
+                horizontal: 40.0,
               ),
-              Flexible(
-                flex: 1,
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Row(
+              child: Center(
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
                     children: [
-                      const Expanded(
-                        child: WelcomeButton(
-                          buttonText: 'Sign in',
-                          tapDestination: 'signin',
-                          color: Colors.transparent,
-                          textColor: Colors.white,
+                      TextSpan(
+                        text: 'Welcome to BeFriends!\n',
+                        style: TextStyle(
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.w600,
+                          color: theme.colorScheme.onPrimary,
                         ),
                       ),
-                      Expanded(
-                        child: WelcomeButton(
-                          buttonText: 'Sign up',
-                          tapDestination: 'signup',
-                          color: Colors.white,
-                          textColor: theme.colorScheme.primary,
+                      TextSpan(
+                        text: '\nA place to connect with people',
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: theme.colorScheme.onPrimary,
                         ),
                       ),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            const Spacer(), // Pushes the buttons to the bottom
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: SizedBox(
+                      height: 40, //height
+                      child: WelcomeButton(
+                        buttonText: 'Sign in',
+                        tapDestination: 'signin',
+                        color: Colors.transparent,
+                        textColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10), //button spacing
+                  Expanded(
+                    child: SizedBox(
+                      height: 50, //
+                      child: WelcomeButton(
+                        buttonText: 'Sign up',
+                        tapDestination: 'signup',
+                        color: Colors.white,
+                        textColor: theme.colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
         ),
       ),
     );
