3dd33e1d683de1ac4803bfb39e1f843d7bd50b25
許裕華 <act23541@gmail.com>
2024-06-22 00:45:59 +0800

    Update the animation of the event pills

--- lib/widget/event_pill.dart
+++ lib/widget/event_pill.dart
@@ -21,13 +21,72 @@ class EventPill extends StatefulWidget {
   final int ppl, maxPpl;
 
   @override
-  State<StatefulWidget> createState() => EventPillState();
+  State<StatefulWidget> createState() => _EventPillState();
 }
 
-class EventPillState extends State<EventPill> {
-  final pillSizeH = 120.0; //how large the pill is vertically
-  final pillPosH = 50.0; //how high the pill is positioned
-  final imgH = 110.0, imgW = 110.0; //how large the image is
+class _EventPillState extends State<EventPill> with TickerProviderStateMixin {
+  final pillSizeH = 120.0; // how large the pill is vertically
+  final pillPosH = 50.0; // how high the pill is positioned
+  final imgH = 110.0, imgW = 110.0; // how large the image is
+
+  late AnimationController _controller;
+  late Animation<double> _fadeInAnimation;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  //method is called once when the state object is created.
+  void initState() {
+    //Calls the parent class's initState method
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000), //1 sec
+      vsync: this, //helps to optimize the animation by preventing offscreen animations from consuming unnecessary resources.
+    );
+
+    _fadeInAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn, //curve, which starts slowly and speeds up.
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.5),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.8,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.elasticOut, //elasticOut curve, which starts quickly, overshoots, and then settles back.
+    ));
+
+    _controller.forward(); //starts the animation
+  }
+
+  @override
+  //use for keep showing the animation eventhough we move from one widget to another
+  void didUpdateWidget(covariant EventPill oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.eventId != oldWidget.eventId) {
+      // Reset the animation controllers
+      _controller.reset();
+      // Restart the animations
+      _controller.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +95,7 @@ class EventPillState extends State<EventPill> {
       bottom: pillPosH,
       left: 20,
       right: 20,
-      duration: Durations.short4,
+      duration: const Duration(milliseconds: 500),
       curve: Curves.easeInOut,
       child: GestureDetector(
         onTap: () {
@@ -44,75 +103,91 @@ class EventPillState extends State<EventPill> {
         },
         child: Align(
           alignment: Alignment.bottomCenter,
-          child: Container(
-            height: pillSizeH,
-            decoration: BoxDecoration(
-              color: theme.colorScheme.background,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  blurRadius: 20,
-                  offset: Offset.zero,
-                  color: Colors.grey.withOpacity(0.5),
-                ),
-              ],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
-                  width: imgW,
-                  height: imgH,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Image.network(
-                      widget.imageURL,
-                      fit: BoxFit.cover,
-                    ),
+          child: MouseRegion(
+            onEnter: (event) {
+              _controller.forward();
+            },
+            onExit: (event) {
+              _controller.reverse();
+            },
+            child: FadeTransition(
+              opacity: _fadeInAnimation,
+              child: ScaleTransition(
+                scale: _scaleAnimation,
+                child: Container(
+                  height: pillSizeH,
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.background,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        blurRadius: 20,
+                        offset: Offset.zero,
+                        color: Colors.grey.withOpacity(0.5),
+                      ),
+                    ],
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 10.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.title,
-                          style: TextStyle(
-                            fontStyle: theme.textTheme.titleSmall!.fontStyle,
-                            fontSize: theme.textTheme.titleSmall!.fontSize,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          overflow: TextOverflow.ellipsis, //need edit
-                        ),
-                        const Divider(),
-                        Text(
-                            'Date: ${widget.date.toString().split(' ')[0]}   ${widget.startTime}'),
-                        Padding(
-                          padding:
-                              const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: theme.colorScheme.tertiaryContainer,
-                                borderRadius: BorderRadius.circular(10.0)),
-                            padding:
-                                const EdgeInsets.fromLTRB(5.0, 3.0, 5.0, 3.0),
-                            child:
-                                Text('Joined: ${widget.ppl}/${widget.maxPpl}'),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                        width: imgW,
+                        height: imgH,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.network(
+                            widget.imageURL,
+                            fit: BoxFit.cover,
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 10.0),
+                          child: SlideTransition(
+                            position: _slideAnimation,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  widget.title,
+                                  style: TextStyle(
+                                    fontStyle: theme.textTheme.titleSmall!.fontStyle,
+                                    fontSize: theme.textTheme.titleSmall!.fontSize,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const Divider(),
+                                Text(
+                                  'Date: ${widget.date.toString().split(' ')[0]}   ${widget.startTime}',
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: theme.colorScheme.tertiaryContainer,
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    padding: const EdgeInsets.fromLTRB(5.0, 3.0, 5.0, 3.0),
+                                    child: Text('Joined: ${widget.ppl}/${widget.maxPpl}'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                    // image
+                    // column
+                    // info
                   ),
                 ),
-              ],
-              //image
-              //column
-              //info
+              ),
             ),
           ),
         ),
